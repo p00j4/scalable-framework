0b86a6e5a0ff1407eea51f2b0e80df135ca6763d
Pooja Shah <writeback2pooja@gmail.com>
2017-05-26 15:34:08 +0530

    "web conflict resolve" github's issue reproduec

--- NewFile.java
+++ NewFile.java
@@ -6,4 +6,11 @@ class NewFile{
   // new line 1
   // new line 2
   // new line 3
+  
+  <<<<<<< qa
+    admin = UsersForDashboardAccess.getUsersForDashboardAccessObjectByFilter(db_name,kwargs={'role':'Admin','active':True},sort=('last_updated', pymongo.ASCENDING))
+=======
+    admins = UsersForDashboardAccess.getUsersForDashboardAccessObjectByFilter(db_name,kwargs={'role':'Admin','active':True},sort=('last_updated', pymongo.ASCENDING))
+    admin = admins[0]
+>>>>>>> dev
 }
